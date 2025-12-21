deb8fc338935652501921d8e635e360b1d6abd43
AzizSOU4BNI <57048839+azizswebni@users.noreply.github.com>
2025-12-21 15:13:38 +0100

    feat: tinymce-editor

---
+++ postinstall.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import fse from 'fs-extra';
+import path from 'path';
+const topDir = import.meta.dirname;
+fse.emptyDirSync(path.join(topDir, 'public', 'tinymce'));
+fse.copySync(path.join(topDir, 'node_modules', 'tinymce'), path.join(topDir, 'public', 'tinymce'), { overwrite: true });
+// Only when using tinymce-premium plugins
+// fse.emptyDirSync(path.join(topDir, 'public', 'tinymce-premium'));
+// fse.copySync(path.join(topDir, 'node_modules', 'tinymce-premium'), path.join(topDir, 'public', 'tinymce-premium'), { overwrite: true });
---
+++ src/app/dashboard/editor/page.tsx
@@ -0,0 +1,9 @@
+import DocEditor from "~/editor";
+
+export default function Page() {
+  return (
+    <div className="px-4 lg:px-6">
+      <DocEditor />
+    </div>
+  );
+}
